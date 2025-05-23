69d3720d3b3500758916b133a86b45f4ee89e274
Oleksandr Palilov <opalilovit@gmail.com>
2025-05-24 00:07:56 +0300

    create structure project and start worl HW

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import SearchBar from "../SearchBar/SearchBar";
+import css from "./App.module.css";
+
+
+export default function App() {
+    const handleSearch = async () => {
+    }
+    return (
+        <div className={css.app}>
+            <SearchBar onSubmit={handleSearch} />
+        </div>
+    )
+}
